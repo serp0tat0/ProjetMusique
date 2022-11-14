cda7cf4f1bd3277599339a5514598caad07f6bcf
serp0tat0 <nathcamaroti@gmail.com>
2022-11-14 10:02:35 -0500

    added a way to input an artist and getting their top 10 tracks, more stuff on the way

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import spotipy
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
+
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
+if len(sys.argv) > 1:
+    name = ' '.join(sys.argv[1:])
+else:
+    name = "Radiohead" #input("insert artist name\n")
+
+results = spotify.search(q='artist:' + name, type='artist')
+items = results['artists']['items']
+if len(items) > 0:
+    artist = items[0]
+
+toptracks = spotify.artist_top_tracks(artist['uri'])
+trackindex = 1
+for track in toptracks['tracks'][:10]:
+
+    print(trackindex, track["name"])
+    trackindex += 1
