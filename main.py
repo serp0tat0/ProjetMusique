120202b9ae3af91e219dc3bcdbab6d5b5d6947de
serp0tat0 <nathcamaroti@gmail.com>
2022-11-29 10:11:06 -0500

    pylast test added, not too sure about that stuff tho

--- main.py
+++ main.py
@@ -2,42 +2,75 @@ import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
+import pylast
 
-"""
 
 
-spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
-if len(sys.argv) > 1:
-    name = ' '.join(sys.argv[1:])
-else:
-    name = input("insert artist name\n") #gives the top 10 for any given artist
+def spotifytest():
+    spotify = spotipy.Spotify(SpotifyOAuth.)
 
-results = spotify.search(q='artist:' + name, type='artist')
-items = results['artists']['items']
-if len(items) > 0:
-    artist = items[0]
+    if len(sys.argv) > 1:
+        name = ' '.join(sys.argv[1:])
+    else:
+        name = input("insert artist name\n") #gives the top 10 for any given artist
 
-toptracks = spotify.artist_top_tracks(artist['uri'])
-trackindex = 1
-for track in toptracks['tracks'][:10]:
+    results = spotify.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
 
-    print(trackindex, track["name"])
-    trackindex += 1
-"""
-scope = "user-library-read"
+    toptracks = spotify.artist_top_tracks(artist['uri'])
+    trackindex = 1
+    for track in toptracks['tracks'][:10]:
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+        print(trackindex, track["name"])
+        trackindex += 1
 
-sp.current_user()
+    scope = "user-library-read"
+    
+    
+    
+    
 
-results = sp.current_user_saved_tracks(limit=50)
-for idx, item in enumerate(results["items"]):
-    idx += 1
-    track = item["track"]
-    print(idx, track["artists"][0]["name"], " - ", track["name"])
-results = sp.current_user_saved_tracks(limit=50, offset=50)
-for idx, item in enumerate(results["items"]):
-    idx += 51
-    track = item["track"]
-    print(idx, track["artists"][0]["name"], " - ", track["name"])
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    sp.current_user()
+
+    results = sp.current_user_saved_tracks(limit=50)
+    for idx, item in enumerate(results["items"]):
+        idx += 1
+        track = item["track"]
+        print(idx, track["artists"][0]["name"], " - ", track["name"])
+    results = sp.current_user_saved_tracks(limit=50, offset=50)
+    for idx, item in enumerate(results["items"]):
+        idx += 51
+        track = item["track"]
+        print(idx, track["artists"][0]["name"], " - ", track["name"])
+
+def lastfmTest():
+    # You have to have your own unique two values for API_KEY and API_SECRET
+    # Obtain yours from https://www.last.fm/api/account/create for Last.fm
+    API_KEY = "b25b959554ed76058ac220b7b2e0a026"  # this is a sample key
+    API_SECRET = "425b55975eed76058ac220b7b4e8a054"
+
+    # In order to perform a write operation you need to authenticate yourself
+    username = "your_user_name"
+    password_hash = pylast.md5("your_password")
+
+    network = pylast.LastFMNetwork(
+        api_key=API_KEY,
+        api_secret=API_SECRET,
+        username=username,
+        password_hash=password_hash,
+    )
+
+    # Now you can use that object everywhere
+    track = network.get_track("Iron Maiden", "The Nomad")
+    track.love()
+    track.add_tags(("awesome", "favorite"))
+
+    # Type help(pylast.LastFMNetwork) or help(pylast) in a Python interpreter
+    # to get more help about anything and see examples of how it works
+
+spotifytest()
