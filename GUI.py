dbcaf9f398549a414696904f276b435ee4508e84
ViveReneLevesque <44099944+serp0tat0@users.noreply.github.com>
2023-04-17 16:18:20 -0400

    Added some juice, working on the GUI now, stuffs pretty fun. Our overall app is kinda done now, like the backend is pretty pimped up, so like, we chilling, we hard chilling. Shits pretty cool, can you believe I was able to do this shit alone? I mean, its really not done yet, I could also add a save feature and keep ur data from old scans, also would try giving full kinda access to spotify api but that shit is way too difficult to deal with, like, have you seen the  demonic shit you need to summon just to have access to how many songs you have liked? Stuffs a little too hard, spotipy has like no documentation and even if it does, nothing gives you access to the amount of times listened to an artist. I should try to get in contact with a programmer who has written something giving access to the API, just to have some insight. Well, I am glad to have my sanity back and seeing progress on this project.

--- GUI.py
+++ GUI.py
@@ -1,18 +1,49 @@
 import tkinter as tk
 from main import login, ILOVEPLAYLISTREADING,SartTesting
 
-window = tk.Tk()
-window.title("Hello Python")
-window.geometry
-button = tk.Button(
-    text="Click me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("1024x576")
+root.resizable(False,False)
+root.title("Button Demo")
+"""
+PUTTING THE BUTTONS HERE BECUSE BIG COPE I CANT PUT THEM AFTER
+"""
+button1= 0
+button2= 0
+button3= 0
+def THE_CREACHURE():
+#uhhgghhhhhhhh yeah
+    button1 = tk.DISABLED
+    button2 = tk.DISABLED
+    button3 = tk.DISABLED
+
+exit_button = ttk.Button(
+    root,
+    text="Exit",
+    command=lambda: root.quit()
 )
-label = tk.Label(text="Name")
-entry = tk.Entry()
-label.pack()
-entry.pack()
-window.mainloop()
+# Create Buttons in the frame
+button1 = tk.Button(root, text="Button-1", command=lambda: THE_CREACHURE)
+button1.place(x=512, y=125)
+
+button2 = tk.Button(root, text="Button-2")
+button2.place(x=512, y=175)
+
+button3 = tk.Button(root, text="Button-3")
+button3.place(x=512, y=225)
+
+#Create a Label
+label = tk.Label(root, text="QM",).pack()
+
+
+#similar artists section
+
+
+
+
+
+
+
+root.mainloop()
