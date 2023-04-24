b1cc20f57525fd13665d58229f97a110885112b0
ViveReneLevesque <44099944+serp0tat0@users.noreply.github.com>
2023-04-24 09:16:16 -0400

    have some more shit now, the GUI now works a minimum, but needs a lil more work done

--- GUI.py
+++ GUI.py
@@ -2,7 +2,14 @@ import tkinter as tk
 from main import login, ILOVEPLAYLISTREADING,SartTesting
 
 from tkinter import ttk
-
+login()
+"""
+FOR SOME REASON ALSO GONNA PUT BUTTON SIZES HERE MAN THIS IS UGLY ASL
+"""
+BWidth1 = 50
+BHeight1 = 15
+BWidth2 = 50
+BHeight2 = 31
 root = tk.Tk()
 root.geometry("1024x576")
 root.resizable(False,False)
@@ -13,7 +20,12 @@ PUTTING THE BUTTONS HERE BECUSE BIG COPE I CANT PUT THEM AFTER
 button1= 0
 button2= 0
 button3= 0
+def switch():
+    if button1["state"] == "normal":
+        button1["state"] = "disabled"
+
 def THE_CREACHURE():
+    print("yeah")
 #uhhgghhhhhhhh yeah
     button1 = tk.DISABLED
     button2 = tk.DISABLED
@@ -25,14 +37,27 @@ exit_button = ttk.Button(
     command=lambda: root.quit()
 )
 # Create Buttons in the frame
-button1 = tk.Button(root, text="Button-1", command=lambda: THE_CREACHURE)
-button1.place(x=512, y=125)
+button1 = tk.Button(root,
+    text="Button-1",
+    height= BHeight1,
+    width= BWidth1,
+    command = ILOVEPLAYLISTREADING)
 
-button2 = tk.Button(root, text="Button-2")
-button2.place(x=512, y=175)
+button1.place(x=128, y=50)
 
-button3 = tk.Button(root, text="Button-3")
-button3.place(x=512, y=225)
+button2 = tk.Button(root,
+    text="Button-2",
+    height=BHeight1,
+    width=BWidth1,
+    command=THE_CREACHURE)
+button2.place(x=128, y=300)
+
+button3 = tk.Button(root,
+    text="Button-3",
+    height=BHeight2,
+    width=BWidth2,
+    command=THE_CREACHURE)
+button3.place(x=512, y=50)
 
 #Create a Label
 label = tk.Label(root, text="QM",).pack()
